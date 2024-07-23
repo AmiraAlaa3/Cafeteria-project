7439f01577001b3c49644f0d7c88bf9ec543623d
Amira Alaa <ameraalaa641@gmail.com>
2024-07-23 23:19:47 +0300

    create user home

--- User/home.php
+++ User/home.php
@@ -1,1 +1,12 @@
 <!-- screen 2 -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+ </head>
+ <body>
+    <h1>Welcome to Home Screen</h1>
+ </body>
+ </html>
